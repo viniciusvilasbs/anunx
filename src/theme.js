57d251a958a49d234f9c73befeedefcbad499d27
Vinicius <vinyvilas@gmail.com>
2021-11-02 21:49:04 -0300

    estrutura inicial + material.ui

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@material-ui/core/styles"
+
+const theme = createTheme ({
+    palette: {
+        primary:{
+            main: '#000000',
+        }
+    }
+})
+
+
+export default theme
