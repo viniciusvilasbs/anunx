346296017376a113bdfaa6b0c7cbd76663fb448c
Vinicius <vinyvilas@gmail.com>
2021-11-07 16:04:43 -0300

    adicionando cards na dashboard do usuario

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,10 +1,23 @@
-import {Button, Container, Typography} from '@material-ui/core'
+import {
+  Button, 
+  Card,
+  CardMedia,
+  CardContent, 
+  Container, 
+  Grid, 
+  Typography,
+  CardActions,
+} from '@material-ui/core'
+
 import {makeStyles} from '@material-ui/core/styles'
 import TemplateDefault from '../../src/templates/Default'
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(8, 0, 6)
+  },
+  cardMedia:{
+    paddingTop: '56%',
   },
   buttonAdd:{
     margin: '30px auto',
@@ -24,6 +37,85 @@ export default function Home() {
           <Button variant="contained" color="primary" className={classes.buttonAdd}>
             Publicar novo anúncio
           </Button>
+      </Container>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}> 
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}> 
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}> 
+            <Card>
+              <CardMedia 
+                className={classes.cardMedia}
+                image={'https://source.unsplash.com/random'}
+                title="Título da imagem"
+              />
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Produto X
+                </Typography>
+                <Typography>
+                  R$60,00
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Editar
+                </Button>
+                <Button size="small" color="primary">
+                  Remover
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </Container>
     </TemplateDefault>
   )
