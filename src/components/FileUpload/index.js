8982d6ada94bf7cff4aa3585f061ce8b2b767c7d
Vinicius <vinyvilas@gmail.com>
2021-11-30 21:06:30 -0300

    publicar anuncio - integracao com API - parte 2

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -16,12 +16,9 @@ const FileUpload = ({files, errors, touched, setFieldValue}) => {
     const {getRootProps, getInputProps} = useDropzone({
         accept: 'image/*',
         onDrop: (acceptedFile) => {
-            const newFiles = acceptedFile.map(file => {
-                return {
-                    ...file,
-                    preview: URL.createObjectURL(file)
-                }
-            })
+            const newFiles = acceptedFile.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            }))
         
             // setFieldValue ('nome', 'valor')
             setFieldValue('files', [
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import formidable from 'formidable-serverless'
 import ProductsModel from '../models/products'
 import dbConnect from '../utils/dbConnect'
@@ -5,12 +7,42 @@ import dbConnect from '../utils/dbConnect'
 const post = async (req, res) => {
     await dbConnect()
 
-    const form = new formidable.IncomingForm()
+    const form = new formidable.IncomingForm({
+        multiples: true,
+        uploadDir: 'public/uploads',
+        keepExtensions: true,
+    })
 
     form.parse(req, (error, fields, data) => {
-        console.log('chegou no form parser')
+        if (error){
+            return res.status(500).json({ success: true})
+        }
 
-        res. status(200)
+        const { files } = data
+
+        const filesToRename = files instanceof Array
+            ? files
+            : [files]
+        
+        filesToRename.forEach(file => {
+            const timestamp = Date.now()
+            const random = Math.floor(Math.random() *99999999) + 1
+            const extention = path.extname(file.name)
+
+            const filename = `${timestamp}_${random}${extention}`
+
+            const oldpath = path.join(__dirname, `../../../../${file.path}`)
+            const newpath = path.join(__dirname, `../../../../${form.uploadDir}/${filename}`)
+
+            fs.rename(oldpath, newpath, (error) => {
+                if (error) {
+                    console.log(error)
+                    return res.status(500).json({ success: true})
+                }
+            })
+        })
+
+        res. status(200).json({ success: true })
     })
 }
 
